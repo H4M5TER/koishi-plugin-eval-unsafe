d5a0d81aaf9df6abe905617296526e779879eabf
H4M5TER <h4m5ter@outlook.com>
2024-09-18 07:47:57 +0800

    v1

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-eval?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-eval)
 
-
+modify from https://gitee.com/DGCK81LNN/lnnbot-miniplugs/blob/master/plugins/common/eval-unsafe.js
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,58 @@
-import { Context, Schema } from 'koishi'
+import { Context, h, z } from 'koishi'
+import { inspect } from 'util'
 
-export const name = 'eval'
+export const name = 'eval-unsafe'
 
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: z<Config> = z.object({})
+
+const AsyncFunction = async function () {}.constructor
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  const logger = ctx.logger(name)
+  logger.level = 3
+
+  ctx.command('eval <code:text>', { 
+    authority: 4,
+    captureQuote: false,
+  }).action(async (argv, code) => {
+    const session = argv.session
+    const expose = {
+      require,
+      ...require('koishi'),
+      ctx,
+      session,
+      // user: session.user,
+      // channel: session.channel,
+      // guild: session.guild,
+      // userId: session.userId,
+      // channelId: session.channelId,
+      // guildId: session.guildId,
+      // selfId: session.selfId,
+      // app: session.app,
+      logger,
+      bot: session.bot,
+      argv,
+      send: session.send.bind(session),
+      // @ts-expect-error
+      exec: (...args) => session.execute(...args, true),
+      inspect,
+    }
+    return (async function __eval_wrapper__() {
+      const func: Function = AsyncFunction(
+        `{ ${Object.keys(expose).join(', ')} }`,
+        `try { return await eval(${JSON.stringify(code)}) } catch (__err) { return "Uncaught " + inspect(__err) }`,
+      )
+      let result = await func.call(this, expose)
+      
+      if (result === undefined) {
+        return ''
+      }
+      if (typeof result !== 'string') {
+        result = inspect(result)
+      }
+      return h.escape(result)
+    })()
+  })
 }
