9e189a6ceaf2af3dc82c9fed8ef4176ad417ff35
H4M5TER <h4m5ter@outlook.com>
2024-09-29 02:45:27 +0800

    v1.0.1

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,17 @@ import { inspect } from 'util'
 
 export const name = 'eval-unsafe'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: z<Config> = z.object({})
 
-const AsyncFunction = async function () {}.constructor
+const AsyncFunction = async function () { }.constructor
 
 export function apply(ctx: Context) {
   const logger = ctx.logger(name)
   logger.level = 3
 
-  ctx.command('eval <code:text>', { 
+  ctx.command('eval <code:text>', {
     authority: 4,
     captureQuote: false,
   }).action(async (argv, code) => {
@@ -37,22 +37,29 @@ export function apply(ctx: Context) {
       send: session.send.bind(session),
       // @ts-expect-error
       exec: (...args) => session.execute(...args, true),
-      inspect,
+      inspect: (obj: any) => inspect(obj, { depth: Infinity }),
     }
-    return (async function __eval_wrapper__() {
+
+    async function __eval_wrapper__() {
       const func: Function = AsyncFunction(
         `{ ${Object.keys(expose).join(', ')} }`,
-        `try { return await eval(${JSON.stringify(code)}) } catch (__err) { return "Uncaught " + inspect(__err) }`,
+        code,
       )
       let result = await func.call(this, expose)
-      
+
       if (result === undefined) {
-        return ''
+        return 'undefined'
       }
       if (typeof result !== 'string') {
         result = inspect(result)
       }
       return h.escape(result)
-    })()
+    }
+
+    try {
+      return __eval_wrapper__()
+    } catch (error) {
+      return 'Uncaught ' + inspect(error)
+    }
   })
 }
